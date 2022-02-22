9de698d9c660e201e65d720d0a32bf9bbeb9d59b
beatrizrpinheiro <beatrizp.gestaoemp@gmail.com>
2022-02-22 15:54:25 -0300

    other entities and second migration

--- SalesWeb/Data/SalesWebContext.cs
+++ SalesWeb/Data/SalesWebContext.cs
@@ -11,5 +11,8 @@ namespace SalesWeb.Data
         }
 
         public DbSet<Department> Department { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+   
     }
 }
